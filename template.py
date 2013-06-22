d21745dc3d63b807ac54c097cb4a69a27675d803
unknown <jamez226@hotmail.com>
2013-06-22 12:52:19 +0100

    Converted cache files to use cStringIO Added basic support for if statements Done a little refactoring on template.py

--- template.py
+++ template.py
@@ -14,17 +14,33 @@ class template:
 	def CachePage(self, filename):
 		print("Caching file %s" % (filename))
 		with open('cache/index.py', 'w') as cache_file:
-			cache_file.write("class index:\n\n\tdef OutputPage(self):\n\t\treturn '''")
+			self._writeHeader(cache_file)
 			with open('templates/index.thtml', 'r') as template_file:
 				content = template_file.read()
 				while content != '':
-					content = re.sub("{([a-zA-Z0-9_-]+)}", "''' + self.\\1 + '''", content)
-					cache_file.write(content)
+					self._writeBlock(cache_file, content)
 					content = template_file.read()
 			
-			cache_file.write("'''")
+			self._writeFooter(cache_file)
 				
-			
+	def _writeHeader(self, stream):
+                stream.write("import cStringIO\n\n")
+                stream.write("class index:\n\n\t")
+                stream.write("def __init__(self):\n\t\tself.buffer = cStringIO.StringIO()\n\n")
+                stream.write("\tdef OutputPage(self):\n\t\tself.buffer.write('''")
+
+        def _writeFooter(self, stream):
+                stream.write("''')\n\n")
+		stream.write("\t\toutput = self.buffer.getvalue()\n")
+		stream.write("\t\tself.buffer.close()\n")
+		stream.write("\t\treturn output\n\n")
+
+	def _writeBlock(self, stream, content):
+                content = re.sub("{([a-zA-Z0-9_-]+)}", "''')\n\t\tself.buffer.write(self.\\1)\n\t\tself.buffer.write('''", content)
+                content = re.sub("<!-- IF ([a-zA-Z0-9\s=\!]+) -->", "''')\n\t\tif \\1:\n\t\t\tself.buffer.write('''", content)
+                content = re.sub("<!-- ELSE -->", "''')\n\t\telse:\n\t\t\tself.buffer.write('''", content)
+                content = re.sub("<!-- ENDIF -->", "''')\n\t\tself.buffer.write('''", content)
+		stream.write(content)
 	
 	def SetVariable(self, name, value):
 		setattr(self.page, name, value)
